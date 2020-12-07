fc90e71a4bc69e96d4e7f3b5be8c9fad4928a6a5
Dmitry Vinogradov <vinogradov.dmitry@gmail.com>
2020-12-07 11:28:34 +0200

    Change phone number in index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use vcamelot\RouteeTest\Notification\Notification;
 
-$notification = new Notification('Dmitry', 'Vinogradov', '+380503723788');
+$notification = new Notification('Dmitry', 'Vinogradov', '+306978745957');
 $result = $notification->run();
 if (!$result) {
     echo $notification->getLastError() . PHP_EOL;
